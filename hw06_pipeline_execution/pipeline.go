6dbd9fea1329126f6186d7171476b3d2653407ce
Little Big <mikhail.anikin15@gmail.com>
2025-11-10 10:02:42 +0300

    feat: hw6 done

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,28 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	for i := 0; i < len(stages); i++ {
+		// Свой выходной канал с возможностью закрытия
+		out := make(Bi)
+		go func(_in In, _out Bi) {
+			defer func() {
+				close(_out)
+				// Нам необходимо "докрутить" канал входных данных, так как только это откроет wg
+				for range _in {} //nolint:all
+			}()
+			for {
+				select {
+				case <-done:
+					return
+				case v, ok := <-_in:
+					if !ok {
+						return
+					}
+					_out <- v
+				}
+			}
+		}(in, out)
+		in = stages[i](out)
+	}
+	return in
 }
