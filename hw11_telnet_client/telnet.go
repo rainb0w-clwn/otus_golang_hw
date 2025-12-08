8a35b12c7e7b85dcce6f1c9ab9e73f123c90838c
Little Big <mikhail.anikin15@gmail.com>
2025-12-08 07:23:16 +0300

    feat: hw11 done

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"errors"
 	"io"
+	"net"
 	"time"
+)
+
+var (
+	ErrConnectionEmpty          = errors.New("telnet connection not opened")
+	ErrConnectionClosedByClient = errors.New("...Connection was closed by client")
+	ErrConnectionClosedByPeer   = errors.New("...Connection was closed by peer")
 )
 
 type TelnetClient interface {
@@ -11,11 +19,62 @@ type TelnetClient interface {
 	Send() error
 	Receive() error
 }
+type telnetClient struct {
+	address string
+	timeout time.Duration
+	conn    net.Conn
+	in      io.ReadCloser
+	out     io.Writer
+}
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+func (c *telnetClient) Connect() error {
+	conn, err := net.DialTimeout("tcp", c.address, c.timeout)
+	if err != nil {
+		return err
+	}
+	errLog.Println("...Connected to " + c.address)
+	c.conn = conn
+	return err
+}
+
+func (c *telnetClient) Send() error {
+	if c.conn == nil {
+		return ErrConnectionEmpty
+	}
+	if _, err := io.Copy(c.conn, c.in); err != nil {
+		return err
+	}
+	errLog.Println(ErrConnectionClosedByClient)
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *telnetClient) Receive() error {
+	if c.conn == nil {
+		return ErrConnectionEmpty
+	}
+	if _, err := io.Copy(c.out, c.conn); err != nil {
+		return err
+	}
+	errLog.Println(ErrConnectionClosedByPeer)
+	return nil
+}
+
+func (c *telnetClient) Close() error {
+	if c.conn == nil {
+		return ErrConnectionEmpty
+	}
+	err := c.conn.Close()
+	if err == nil {
+		c.conn = nil
+	}
+	return err
+}
+
+func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
+	return &telnetClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
--- hw11_telnet_client/telnet_test.go
+++ hw11_telnet_client/telnet_test.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net"
 	"sync"
+	"syscall"
 	"testing"
 	"time"
 
@@ -61,5 +62,80 @@ func TestTelnetClient(t *testing.T) {
 		}()
 
 		wg.Wait()
+	})
+	t.Run("no server", func(t *testing.T) {
+		client := NewTelnetClient(
+			"0.0.0.0:8000",
+			time.Second*10,
+			io.NopCloser(&bytes.Buffer{}),
+			&bytes.Buffer{},
+		)
+		err := client.Connect()
+		require.Error(t, err)
+	})
+	t.Run("incorrect usage", func(t *testing.T) {
+		client := NewTelnetClient("0.0.0.0:8000", time.Second*10, io.NopCloser(&bytes.Buffer{}), &bytes.Buffer{})
+		sendErr := client.Send()
+		require.ErrorIs(t, sendErr, ErrConnectionEmpty)
+		receiveErr := client.Receive()
+		require.ErrorIs(t, receiveErr, ErrConnectionEmpty)
+		closeErr := client.Close()
+		require.ErrorIs(t, closeErr, ErrConnectionEmpty)
+	})
+	t.Run("eof test", func(t *testing.T) {
+		l, err := net.Listen("tcp", "0.0.0.0:")
+		require.NoError(t, err)
+		defer func() { require.NoError(t, l.Close()) }()
+		wg := sync.WaitGroup{}
+		wg.Add(2)
+		go func() {
+			defer wg.Done()
+			in := &bytes.Buffer{}
+			client := NewTelnetClient(l.Addr().String(), time.Second*10, io.NopCloser(in), &bytes.Buffer{})
+			require.NoError(t, client.Connect())
+			defer func() { require.NoError(t, client.Close()) }()
+			in.WriteString("hello\u0000")
+			err = client.Send()
+			require.NoError(t, err)
+		}()
+		go func() {
+			defer wg.Done()
+			conn, err := l.Accept()
+			require.NoError(t, err)
+			require.NotNil(t, conn)
+			defer func() { require.NoError(t, conn.Close()) }()
+			request := make([]byte, 1024)
+			n, err := conn.Read(request)
+			require.NoError(t, err)
+			require.Equal(t, "hello\u0000", string(request)[:n])
+			_, err = conn.Read(request)
+			require.Equal(t, err, io.EOF)
+		}()
+		wg.Wait()
+	})
+	t.Run("connection timeout", func(t *testing.T) {
+		l, err := net.Listen("tcp", "0.0.0.0:")
+		require.NoError(t, err)
+		defer func() { require.NoError(t, l.Close()) }()
+		client := NewTelnetClient(l.Addr().String(), time.Nanosecond*1, io.NopCloser(&bytes.Buffer{}), &bytes.Buffer{})
+		require.Error(t, client.Connect())
+	})
+	t.Run("send/receive to closed connection", func(t *testing.T) {
+		l, err := net.Listen("tcp", "0.0.0.0:")
+		require.NoError(t, err)
+		in := &bytes.Buffer{}
+		client := NewTelnetClient(l.Addr().String(), time.Second*10, io.NopCloser(in), &bytes.Buffer{})
+		require.NoError(t, client.Connect())
+		require.NoError(t, l.Close())
+		in.WriteString("hello\n")
+		require.ErrorIs(t, client.Send(), syscall.ECONNRESET)
+	})
+	t.Run("send/receive to closed connection", func(t *testing.T) {
+		l, err := net.Listen("tcp", "0.0.0.0:")
+		require.NoError(t, err)
+		client := NewTelnetClient(l.Addr().String(), time.Second*10, io.NopCloser(&bytes.Buffer{}), &bytes.Buffer{})
+		require.NoError(t, client.Connect())
+		require.NoError(t, l.Close())
+		require.Error(t, client.Receive(), syscall.ECONNRESET)
 	})
 }
