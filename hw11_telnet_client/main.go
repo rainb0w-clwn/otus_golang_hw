65dbd98c820615b5d81148d82912f8697fe24cac
rainb0w-clwn <61455796+rainb0w-clwn@users.noreply.github.com>
2025-12-16 10:32:39 +0300

    feat: hw11 done

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,46 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var errLog = log.New(os.Stderr, "", 0)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	timeout := flag.Duration("timeout", 10*time.Second, "timeout")
+	flag.Parse()
+	if len(flag.Args()) != 2 {
+		errLog.Println("args validation error")
+		return
+	}
+	host := flag.Arg(0)
+	port := flag.Arg(1)
+	rootCtx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT)
+	defer stop()
+	client := NewTelnetClient(net.JoinHostPort(host, port), *timeout, os.Stdin, os.Stdout)
+	err := client.Connect()
+	if err != nil {
+		errLog.Println(fmt.Errorf("client connection error: %w", err))
+		return
+	}
+	defer func() {
+		_ = client.Close()
+	}()
+	go func() {
+		_ = client.Send()
+		stop()
+	}()
+	go func() {
+		_ = client.Receive()
+		stop()
+	}()
+	<-rootCtx.Done()
 }
