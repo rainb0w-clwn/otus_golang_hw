627b9b1f3ed1ed5a29304deaf44e0bfa75a29445
rainb0w-clwn <61455796+rainb0w-clwn@users.noreply.github.com>
2025-12-16 10:02:20 +0300

    feat: hw7 done

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,82 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
-	ErrUnsupportedFile       = errors.New("unsupported file")
-	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrUnsupportedOffsetLimit = errors.New("unsupported offset or limit")
+	ErrFromFileNotExists      = errors.New("input file not exists")
+	ErrFromFileOpen           = errors.New("input file opening error")
+	ErrFromFileUnsupported    = errors.New("input file not supported")
+	ErrOffsetExceedsFileSize  = errors.New("offset exceeds file size")
+	ErrToFileDirNotExists     = errors.New("out file dir not exists")
+	ErrToFileOpen             = errors.New("out file opening error")
+	ErrToFileWrite            = errors.New("out file writing error")
 )
 
-func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
-	return nil
+func Copy(fromPath, toPath string, offset, limit int64) (int64, error) {
+	if offset < 0 || limit < 0 {
+		return 0, ErrUnsupportedOffsetLimit
+	}
+
+	fromFile, err := os.Open(fromPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return 0, ErrFromFileNotExists
+		}
+		return 0, ErrFromFileOpen
+	}
+	defer fromFile.Close()
+
+	fromFileStat, err := fromFile.Stat()
+	switch {
+	case err != nil:
+		return 0, err
+	case fromFileStat.IsDir():
+		return 0, ErrFromFileUnsupported
+	}
+
+	fromFileRegular := fromFileStat.Mode().IsRegular()
+	fromFileSize := fromFileStat.Size()
+
+	if fromFileRegular {
+		if offset > fromFileSize {
+			return 0, ErrOffsetExceedsFileSize
+		}
+	} else if limit == 0 {
+		return 0, ErrFromFileUnsupported
+	}
+
+	toFile, err := os.OpenFile(toPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o644)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return 0, ErrToFileDirNotExists
+		}
+		return 0, ErrToFileOpen
+	}
+	defer toFile.Close()
+
+	reader := io.Reader(fromFile)
+	if offset > 0 {
+		if _, err = fromFile.Seek(offset, io.SeekStart); err != nil {
+			return 0, err
+		}
+	}
+
+	bytesToRead := fromFileSize - offset
+
+	if limit > 0 && (!fromFileRegular || fromFileSize >= offset+limit) {
+		bytesToRead = limit
+	}
+
+	bar := pb.Full.Start64(bytesToRead)
+	defer bar.Finish()
+
+	barReader := bar.NewProxyReader(reader)
+
+	return io.CopyN(toFile, barReader, bytesToRead)
 }
--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"errors"
 	"flag"
+	"fmt"
+	"os"
 )
 
 var (
@@ -12,11 +15,45 @@ var (
 func init() {
 	flag.StringVar(&from, "from", "", "file to read from")
 	flag.StringVar(&to, "to", "", "file to write to")
-	flag.Int64Var(&limit, "limit", 0, "limit of bytes to copy")
-	flag.Int64Var(&offset, "offset", 0, "offset in input file")
+	flag.Int64Var(&limit, "limit", 0, "limit of bytes to copy >= 0")
+	flag.Int64Var(&offset, "offset", 0, "offset in input file >= 0")
 }
 
 func main() {
 	flag.Parse()
-	// Place your code here.
+
+	if from == "" {
+		fmt.Println("The 'from' flag is required.")
+		flag.PrintDefaults()
+		return
+	}
+
+	if to == "" {
+		fmt.Println("The 'to' flag is required.")
+		flag.PrintDefaults()
+		return
+	}
+
+	if offset < 0 {
+		fmt.Println("The 'offset' flag is unsupported.")
+		flag.PrintDefaults()
+		return
+	}
+
+	if limit < 0 {
+		fmt.Println("The 'limit' flag is unsupported.")
+		flag.PrintDefaults()
+		return
+	}
+
+	_, err := Copy(from, to, offset, limit)
+	if err != nil {
+		if errors.Is(err, ErrUnsupportedOffsetLimit) {
+			fmt.Printf("Error: %s\n", err.Error())
+			flag.PrintDefaults()
+		} else {
+			_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		}
+		os.Exit(1)
+	}
 }
