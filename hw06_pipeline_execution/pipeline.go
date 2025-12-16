a89aa1dfcf2d70345bc82ce278d37d6a02b9c355
rainb0w-clwn <61455796+rainb0w-clwn@users.noreply.github.com>
2025-12-16 10:00:11 +0300

    feat: hw6 done
    * feat: hw6 done

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,35 @@ type (
 
 type Stage func(in In) (out Out)
 
+func drain(ch In) {
+	for range ch {} //nolint:all
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	for _, stage := range stages {
+		stageOut := stage(in)
+		out := make(Bi)
+		go func(_in In, _out Bi) {
+			defer close(_out)
+			for {
+				select {
+				case <-done:
+					go drain(_in)
+					return
+				case v, ok := <-_in:
+					if !ok {
+						return
+					}
+					select {
+					case <-done:
+						go drain(_in)
+						return
+					case _out <- v:
+					}
+				}
+			}
+		}(stageOut, out)
+		in = out
+	}
+	return in
 }
